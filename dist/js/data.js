d85645d893dcb8302e757765a4144d0382665cb5
Hov <asatryanhovhannes1995@gmail.com>
2023-07-16 13:44:18 +0200

    add js module and btn flex

---
+++ dist/js/data.js
@@ -0,0 +1,8 @@
+export const data = {
+  tab1 : {
+    btn: "present simple",
+    descr: "l am you are",
+  },
+  
+};
+
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,6 @@
+import {data} from "./data.js";
+console.log(data.tab1.descr);
+
 document.addEventListener("DOMContentLoaded", function() {
   const tabButtons = document.querySelectorAll(".tab-button");
   const tabs = document.querySelectorAll(".tab");
@@ -18,4 +21,23 @@ document.addEventListener("DOMContentLoaded", function() {
       tabs[index].classList.add("active");
     });
   });
+  // const data = {
+  //   tab1 : {
+  //     btn: "present simple",
+  //     descr: "l am you are",
+  //   },
+  
+  // };
+
+  let btn1 = document.querySelector("#btn1");
+  let descr1 = document.querySelector("#descr1");
+
+  btn1.innerHTML = data.tab1.btn;
+  descr1.innerHTML = data.tab1.descr;
+
+
+
+
 });
+
+
